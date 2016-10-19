cfcccaf4d0f1b567fba0e2edbcfd363e15171d18
shegoj <shegoj@yahoo.com>
2016-10-19 19:10:26 -0400

    updated

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -1,5 +1,7 @@
 class profile::webserver {
   #include ntp
+	class { 'nginx': }
+
 	nginx::resource::vhost { 'xwebserver.vagrant.vm':
   listen_port => 80,
   proxy       => 'http://localhost:5601',
