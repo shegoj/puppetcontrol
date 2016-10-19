74a8d71ba9132cf9571282982177193bd73bd78f
shegoj <shegoj@yahoo.com>
2016-10-19 18:54:09 -0400

    updated

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -3,5 +3,12 @@ class profile::webserver {
 	nginx::resource::vhost { 'xwebserver.vagrant.vm':
   listen_port => 80,
   proxy       => 'http://localhost:5601',
+  proxy_set_header => [
+    'Host $host',
+    'X-Real-IP $remote_addr',
+    'X-Forwarded-For $proxy_add_x_forwarded_for',
+    'Proxy ""',
+  ],
+	
 	}	
 }
