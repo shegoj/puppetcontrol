ebc128709efd5d11bc79cd20ace9341ca6f27fe7
Olu <shegoj@yahoo.com>
2016-11-10 22:32:58 -0500

    updated

--- site/profile/manifests/zabbix/master.pp
+++ site/profile/manifests/zabbix/master.pp
@@ -4,4 +4,14 @@ class profile::zabbix::master {
   file {'/tmp/do.txt' :
     ensure  => present,
   }
+  class { 'apache':
+    mpm_module => 'prefork',
+  }
+  include apache::mod::php
+
+  class { 'postgresql::server': }
+
+  class { 'zabbix':
+    zabbix_url    => 'zabbix.example.com',
+  }
 }
