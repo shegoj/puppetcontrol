4ce27709a7b270464949ddcf0846c6c6e630ba23
root <root@UNKNOWN.Home>
2016-11-13 12:10:20 +0000

    updated

--- site/profile/manifests/wordpress/master.pp
+++ site/profile/manifests/wordpress/master.pp
@@ -1,5 +1,8 @@
 class profile::wordpress::master {
-  class { 'wordpress': }
+  class { 'wordpress': 
+    version => '4.6'
+  }
+  include '::mysql::server'
   notify {' will install wordpress master' : }
 }
 
